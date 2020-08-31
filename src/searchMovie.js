7ccf74d1c7d54243feb19f4e795e3045d552522c
idrissiradi <idrissiradi@gmail.com>
2020-08-31 11:48:29 +0100

    start again

--- src/searchMovie.js
+++ src/searchMovie.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import MovieCard from "./movieCard";
 
 export default function SearchMovie() {
 
@@ -26,15 +27,7 @@ export default function SearchMovie() {
             </form>
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => (
-                    <div className="card" key={movie.id}>
-                        <img className="card--image" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} alt={movie.title + ' poster'} />
-                        <div className="card--content">
-                            <h3 className="card--title">{movie.title}</h3>
-                            <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                            <p><small>RATING: {movie.vote_average}</small></p>
-                            <p className="card--desc">{movie.overview}</p>
-                        </div>
-                    </div>   
+                    <MovieCard movie={movie} key={movie.id}/>
                 ))}
             </div>
         </>
